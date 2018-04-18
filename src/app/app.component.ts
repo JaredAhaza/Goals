46ab1801e22501c6724bfcbdde57baad3a4e20b7
JaredAhaza <jaredahazq@gmail.com>
2018-04-18 12:25:05 +0300

    modified page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
+// import { Component } from '@angular/core';
+//
+// @Component({
+//   selector: 'app-root',
+// //   templateUrl: './app.component.html',
+// //   styleUrls: ['./app.component.css']
+// // })
+// // export class AppComponent {
+// //   title = 'app';
+// // }
+// // import { Component } from '@angular/core';
+// //
+// // @Component({
+// //   selector: 'app-root',
+// //   template: `
+// //     <h1> My Goals </h1>
+// //     <p> My goal for today is {{goal}} </p>
+// //   `
+// // })
+// // export class AppComponent {
+// //   goal = 'Watch Finding Nemo';
+// // }
+// // import { Component } from '@angular/core';
+// //
+// // @Component({
+// //   selector: 'app-root',
+// //   templateUrl: './app.component.html'
+// // })
+// // export class AppComponent {
+// //   goal = 'Watch Finding Nemo';
+// // }
+// //
 import { Component } from '@angular/core';
+ import {Goal} from './goal'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   selector: 'app-root',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+  //goals = [
+    //     new Goal(1,'Watch Finding Nemo'),
+    //     new Goal(2,'Buy Cookies'),
+    //     new Goal(3, 'Get new Phone Case'),
+    //     new Goal(4, 'Get Dog Food'),
+    //     new Goal(5, 'Solve math homework'),
+    //     new Goal(6, 'Plot my world domination plan'),
+    //
+    // ]
 }
